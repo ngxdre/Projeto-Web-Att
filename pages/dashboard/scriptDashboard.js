bfdc08e661c0a8104570cc0109ed2ebfd2ccdaeb
André Santos da Silva <andreew.0003@gmail.com>
2024-05-24 02:44:47 -0300

    feat: Adiciona funcionalidade nas paginas de Dashboard

--- pages/dashboard/scriptDashboard.js
+++ pages/dashboard/scriptDashboard.js
@@ -1,3 +1,19 @@
-function teste(){
-    alert('funfou');
+function matricula(){
+    window.location.replace("/pages/dashboard_cursosMat/paginaCursosMat.html");
+}
+
+function disponiveis(){
+    window.location.replace("/pages/dashboard_cursosDisp/paginaCursosDisp.html");
+}
+
+function feedbacks(){
+    window.location.replace("/pages/dashboard_feedback/paginaFeedback.html");
+}
+
+function areaProf(){
+    window.location.replace("/pages/dashboard_prof/paginaProf.html");
+}
+
+function suporte(){
+    window.location.replace("/pages/dashboard_suporte/paginaSuporte.html");
 }
